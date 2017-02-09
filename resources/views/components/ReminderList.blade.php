29a65d13d9649d5bb972ae3af991549b51f5f557
korn <kornchaval@gmail.com>
2017-02-09 19:29:12 +0700

    Add view ReminderList.blade.php and edit view home.blade.php

---
+++ resources/views/components/ReminderList.blade.php
@@ -0,0 +1,13 @@
+
+<div class ="row">
+      <h2>Your Reminder(s)</h2>
+</div>
+
+<div class ="row">
+  @foreach ($reminders as $reminder)
+    <div class="well">
+      <span>{{$reminder}}</span>
+      <a class = "btn btn-success pull-right">Finish</a>
+    </div>
+  @endforeach
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,9 @@
 
 @section('content')
   <div class="container">
-    <div class ="row">
-    <h2>Your Reminder(s)</h2>
-    </div>
-
-    <div class ="row">
-      @foreach ($reminders as $reminder)
-        <div class="well">
-          <span>{{$reminder}}</span>
-          <a class = "btn btn-success pull-right">Finish</a>
-        </div>
-      @endforeach
 
 
-    </div>
-
+    @include('components.ReminderList',['reminders'=>$reminders])
 
 
   @include('components.NewReminder')
