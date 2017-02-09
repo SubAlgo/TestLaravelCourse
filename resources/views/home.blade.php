d4ae2253de55164497480383f96c2ea925669c58
korn <kornchaval@gmail.com>
2017-02-09 16:49:31 +0700

    Add Your Reminder(s)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,21 @@
 @extends('template')
 
 @section('content')
+  <div class="container">
+    <div class ="row">
+      <h2>Your Reminder(s)</h2>
+    </div>
+
+    <div class ="row">
+      <div class="well">
+        <span>Buy Milk</span>
+        <a class = "btn btn-success pull-right">Finish</a>
+      </div>
+    </div>
+
+
+
   @include('components.NewReminder')
+
+</div>
 @endsection
