0b02c28f4549e43626b13de9f6b7e9024bd10e77
korn <kornchaval@gmail.com>
2017-02-19 16:07:21 +0700

    Start : softDeletes [เริ่มทำ softDeletes]

--- app/ReminderModel.php
+++ app/ReminderModel.php
@@ -3,9 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ReminderModel extends Model
 {
+  use SoftDeletes;
     protected $table = "Reminder"; //บรรทัดนี้ไม่จำเป็นต้องเขียนก็ได้ ถ้าชื่อ Class ของเราคือ Reminder
     public $primarykey ="id";
     public $timestamp = true; //ละได้เหมือนกัน
--- resources/views/components/ReminderTypeList.blade.php
+++ resources/views/components/ReminderTypeList.blade.php
@@ -7,7 +7,7 @@
         {{--<a class="btn btn-success pull-right">Finish</a>--}}
         <form method="post" action="/reminder/delete">
           <div class="well" >
-            <span><b>({{$reminderType->typename}})</b></span>
+            <span><b>{{$reminderType->typename}}</b></span>
           </div>
         </form>
     @endforeach
