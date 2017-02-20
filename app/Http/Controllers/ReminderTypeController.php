730116d6d61daf95c14e4f77c40f67006a9577ea
korn <kornchaval@gmail.com>
2017-02-20 20:51:21 +0700

    validate typename

--- app/Http/Controllers/ReminderTypeController.php
+++ app/Http/Controllers/ReminderTypeController.php
@@ -6,10 +6,19 @@ use Illuminate\Http\Request;
 
 use App\ReminderTypeModel;
 
+
 class ReminderTypeController extends Controller
 {
     public function addReminderType (Request $request)
     {
+      //$validator = Validator::make($request->all(), ['name' =>'request|uniques:ReminderType']);
+
+      //Validate typename ถ้ามีข้อมูลซ้ำจะไม่ถูกบันทึกซ้ำ
+      //ตรวจสอบ name ที่ถูกส่งค่ามาจาก form โดยตรวจจาก ฟิว typename ของ Table ReminderType 
+      $this->validate($request,[
+        'name'=> 'required|unique:ReminderType,typename'
+      ]);
+
       $typeName = $request->name;
 
       $reminderType = new ReminderTypeModel();
